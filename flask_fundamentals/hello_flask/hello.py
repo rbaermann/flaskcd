8dac61c8ea0d70d96641c2049b5d7b3e6692ac69
rbaermann <rcbaermann@gmail.com>
2019-07-03 15:33:39 -0700

    I added more info to my hello_flask file

--- flask_fundamentals/hello_flask/hello.py
+++ flask_fundamentals/hello_flask/hello.py
@@ -8,6 +8,16 @@ def index():
 def success():
     return 'success'
 
+@app.route('/lists')
+def render_lists():
+    student_info = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30},
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template('lists.html', random_numbers = [3,1,5], students = student_info)
+
 @app.route('/hello/<name>')
 def hello(name):
     print(name)
